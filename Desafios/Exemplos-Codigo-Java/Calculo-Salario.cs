64f28dd26eeacf7cdab008cbaa4e79d881b5d84b
Arlete D' Lira <deliraarlete@gmail.com>
2025-10-02 13:08:59 -0300

    Create Calculo-Salario.cs

---
+++ Desafios/Exemplos-Codigo-Java/Calculo-Salario.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Desafio
+{
+    public static void Main()
+    {
+        //Lê os valores de entrada:
+        float valorSalario = float.Parse(Console.ReadLine());
+        float valorBeneficios = float.Parse(console.ReadLine());
+
+        float valorImposto = 0;
+        if (valorSalario >= 0 && valorSalario <= 1100)
+        {
+            //Atribui a aliquota de 5% mediante o salário:
+            valorImposto = 0.05F * valorSalario;
+        }
+        else if (valorSalario >= 1100.01 && valorSalario <= 2500.00)
+        {
+            valorImposto = 0.10F * valorSalario;
+        }
+        else
+        {
+            valorImposto = 0.15F * valorSalario;
+        }
+
+        //calcula e imprime a Saída (com 2 casas decimais)
+        float saida = valorSalario - valorImposto + valorBeneficios;
+        Console.WriteLine(saida.ToString("0.00"));
+    }
+
+}
